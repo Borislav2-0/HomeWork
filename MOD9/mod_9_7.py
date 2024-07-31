ae130b33be28b7e5c31c1d0fa68c42d719f6350c
Borislav2-0 <crva20@mail.ru>
2024-07-31 14:18:56 +0300

    Add files via upload

--- MOD9/mod_9_7.py
+++ MOD9/mod_9_7.py
@@ -1,35 +1,16 @@
-"""Цель задания:
-    Освоить механизмы создания декораторов Python.
-    Практически применить знания, создав функцию декоратор и обернув ею другую функцию.
-
-Задание:
-    Напишите 2 функции:
-        Функция, которая складывает 3 числа (sum_three)
-        Функция декоратор (is_prime), которая распечатывает "Простое", если результат 1ой функции будет простым числом и "Составное" в противном случае.
-
-Пример:
-result = sum_three(2, 3, 6)
-print(result)
-
-Результат консоли:
-Простое
-11
-
-Примечания:
-    Не забудьте написать внутреннюю функцию wrapper в is_prime
-    Функция is_prime должна возвращать wrapper
-    @is_prime - декоратор для функции sum_three
-"""
+import math
 
 
 def is_prime(func):
     def wrapper(a, b, c):
         res = func(a, b, c)
-        if type(res) is int:
+        if res > 1:
+            for i in range(2, int(math.sqrt(res)) + 1):
+                if res % i == 0:
+                    print(f'The number {res} is a composite')
+                    return res
             print(f'The number {res} is a prime')
-        else:
-            print(f'The number {res} is a composite')
-        return res
+            return res
 
     return wrapper
 
@@ -40,7 +21,14 @@ def sum_three(a, b, c):
     return result
 
 
-result = sum_three(2, 3, 6)
+result = sum_three(8, 3, 6)
 print(result)
-result = sum_three(2, 90.625, 6)
+
+result = sum_three(6, 9, 7)
+print(result)
+
+result = sum_three(1, 9, 615)
+print(result)
+
+result = sum_three(899, 6, 2)
 print(result)
