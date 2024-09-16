33ab82356db5ad40d5fc0a53bbbc8f75689bbf15
Borislav2-0 <crva20@mail.ru>
2024-09-16 06:13:11 +0300

    Домашнее задание по теме "Машина состояний"

--- MOD13/mod_13_4.py
+++ MOD13/mod_13_4.py
@@ -3,7 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
-api = "7106153589:AAHwMapuxsSAI1ntqJnq-zTXKoxQ4w1pvCM"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
